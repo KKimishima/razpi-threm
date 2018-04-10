172e2630abbcd6db03a44d6634818a4ff2a4cdc2
君島克弥 <K.Kimishima123@gmail.com>
2018-04-10 23:59:59 +0900

    <追加>トップページを返すコントローラ作製 refs #81

---
+++ src/main/java/com/github/kkimishima/razpithrem/PageController.java
@@ -0,0 +1,12 @@
+package com.github.kkimishima.razpithrem;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class PageController {
+  @RequestMapping(value = "/")
+  public String index(){
+    return "index";
+  }
+}
