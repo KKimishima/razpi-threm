99346680bb4d26b01ac9e12bb9f3cd303aac9750
君島克弥 <K.Kimishima123@gmail.com>
2018-04-10 21:19:33 +0900

    <追加>初期設定追加

---
+++ src/main/java/com/github/kkimishima/razpithrem/RazpiThremApplication.java
@@ -0,0 +1,12 @@
+package com.github.kkimishima.razpithrem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RazpiThremApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(RazpiThremApplication.class, args);
+  }
+}
