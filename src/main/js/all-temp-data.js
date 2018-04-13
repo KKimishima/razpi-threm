660a93f96c39e5fb702695a9056a9c268b5413d3
君島克弥 <K.Kimishima123@gmail.com>
2018-04-13 20:00:10 +0900

    <追加>ページのjs作製

---
+++ src/main/js/all-temp-data.js
@@ -0,0 +1,20 @@
+import jQuery from "jquery";
+
+const $ = jQuery;
+
+export function all_data(all_temp,err) {
+  $.ajax({
+    url:"/api/v1/temp/",
+    type:"GET",
+    dataType:"json"
+  }).done((result)=>{
+    for(let i  of result){
+      all_temp.append(`<tr><td>${i["no"]}</td><td>${i["datetime"]}</td><td>${i["temp"]}</td></tr>`);
+    }
+  }).fail(()=>{
+    err.html(`
+      <div class="alert alert-danger" role="alert">
+        <strong>読み込み失敗</strong> サーバに接続できません
+      </div>`)
+  })
+}
---
+++ src/main/js/index.js
@@ -0,0 +1,31 @@
+import "../../../node_modules/bootstrap/dist/css/bootstrap.min.css"
+import jQuery from "jquery";
+import "bootstrap"
+import "popper.js"
+import "./common.css"
+import {lasttempo} from "./init-last-temp";
+import {all_data} from "./all-temp-data";
+import {last_t_tempo} from "./init-last-temp";
+
+const $ = jQuery;
+
+// 2分ごとの読み込み
+function load_temp() {
+  $(() => {
+    setInterval(() => {
+      console.log("定期読み込み");
+      lasttempo($("#last-temp"),$("#all-temp"));
+      last_t_tempo($("#all-temp"),$("#error-msg"));
+    }, 120000)
+  });
+}
+
+// var temp = $("#last-temp");
+// 初回読み込み
+$(document).ready(() =>{
+  console.log("初回読み込み");
+  lasttempo($("#last-temp"));
+  all_data($("#all-temp"),$("#error-msg"));
+  load_temp()
+});
+
